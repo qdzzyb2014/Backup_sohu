72186b3442b81d69f70c377e47358ae350ba0c4c
王林基 <313766556@qq.com>
2016-01-26 15:33:49 +0800

    add unit test

--- test.py
+++ test.py
@@ -1,12 +1,13 @@
+# -*- coding: utf-8 -*-
 import mock
 import unittest
 
-import requests
-from bs4 import BeautifulSoup
+from StringIO import StringIO
+
 
 import backup
 import main
-from config import BASE_DIR, TIMER, URL, BACKUP_DIR, DIRNAMES
+from config import BASE_DIR, BACKUP_DIR, DIRNAMES
 
 
 class BackUpTestCase(unittest.TestCase):
@@ -14,6 +15,8 @@ class BackUpTestCase(unittest.TestCase):
     def setUp(self):
         backup.urllib = mock.Mock()
         backup.os = mock.Mock()
+        self.path = 'path'
+        self.soup = mock.Mock()
 
     def test_is_abs_url(self):
         # Test is_abs_url
@@ -26,8 +29,112 @@ class BackUpTestCase(unittest.TestCase):
         url = '/%7Eguido/Python.html'
         self.assertEqual(backup.is_abs_url(url), '')
 
-    def test_create_external_file(self):
-        pass
+    @mock.patch('backup.urllib')
+    def test_create_external_file(self, mock_urllib):
+        t_tag = mock.Mock()
+        t_tag.get.return_value = 'target'
+        get_arg = 'src'
+        path = 'path'
+        backup.os.path.join.return_value = 'filepath'
+        with mock.patch('backup.os.path.exists', return_value=True):
+            backup.create_external_file(t_tag, get_arg, path)
+            t_tag.get.assert_called_with(get_arg)
+            self.assertFalse(mock_urllib.urlretrieve.called)
+
+        with mock.patch('backup.os.path.exists', return_value=False):
+            backup.create_external_file(t_tag, get_arg, path)
+            mock_urllib.urlretrieve.assert_called_with(
+                'target', 'filepath')
+
+    def test_create_inline_file(self):
+        mock_open = mock.mock_open()
+        t_tag = mock.Mock()
+        t_tag.text = 'text'
+        backup.os.path.join.return_value = 'final path'
+        with mock.patch('backup.open', mock_open):
+            backup.create_inline_file(t_tag, 'path', 'fe', 1)
+
+            mock_open.assert_called_once_with('final path', 'wb')
+            mock_open().write.assert_called_once_with('text')
+
+    @mock.patch('sys.stdout', new_callable=StringIO)
+    def test_html_backup(self, mock_stdout):
+        mock_open = mock.mock_open()
+        self.soup.prettify.return_value = 'entire html'
+        backup.os.path.join.return_value = 'html path'
+        with mock.patch('backup.open', mock_open):
+            backup.html_backup(self.soup, self.path)
+
+            mock_open.assert_called_once_with('html path', 'wb')
+            mock_open().write.assert_called_once_with('entire html')
+            self.assertTrue(mock_stdout.getvalue() == 'HTML has backed up.\n')
+
+    @mock.patch('backup.create_external_file')
+    @mock.patch('sys.stdout', new_callable=StringIO)
+    def test_images_backup(self, mock_stdout, mock_create_external_file):
+        self.soup.find_all.return_value = []
+        backup.images_backup(self.soup, self.path)
+        self.assertFalse(mock_create_external_file.called)
+        self.assertTrue(mock_stdout.getvalue() == 'There is no images.\n')
+
+        mock_create_external_file.reset_mock()
+        self.soup.find_all.return_value = [1, 2, 3]
+
+        backup.images_backup(self.soup, self.path)
+        self.soup.find_all.assert_called_with('img')
+        backup.os.path.join.assert_called_with(self.path, 'images')
+        self.assertTrue(
+            mock_create_external_file.call_count ==
+            len(self.soup.find_all()))
+        self.assertTrue(
+            mock_stdout.getvalue().endswith('Iamges had backed up.\n'))
+
+    @mock.patch('backup.create_inline_file')
+    @mock.patch('backup.create_external_file')
+    @mock.patch('sys.stdout', new_callable=StringIO)
+    def test_css_backup(self, mock_stdout, mock_ex_file, mock_inline_file):
+        self.soup.find_all.return_value = []
+        backup.css_backup(self.soup, self.path)
+        self.assertFalse(mock_ex_file.called)
+        self.assertFalse(mock_inline_file.called)
+
+        self.soup.find_all.return_value = [1, 2, 3]
+        mock_ex_file.reset_mock()
+        mock_inline_file.reset_mock()
+        backup.css_backup(self.soup, self.path)
+        self.assertTrue(
+            mock_ex_file.call_count == mock_inline_file.call_count)
+        self.assertTrue(
+            mock_stdout.getvalue().endswith('CSS has backed up.\n'))
+
+    @mock.patch('backup.create_inline_file')
+    @mock.patch('backup.create_external_file')
+    @mock.patch('sys.stdout', new_callable=StringIO)
+    @mock.patch('bs4.Tag')
+    def test_js_backup(self, mock_tag, mock_stdout,
+                       mock_ex_file, mock_inline_file):
+        # init
+        mock_tag.has_attr.return_value = True
+        js_list = []
+
+        # if no js
+        self.soup.find_all.return_value = js_list
+        backup.js_backup(self.soup, self.path)
+        self.assertTrue(mock_stdout.getvalue() == 'There is no js.\n')
+        self.assertFalse(mock_ex_file.called)
+        self.assertFalse(mock_inline_file.called)
+
+        # else
+        for i in xrange(5):
+            js_list.append(mock_tag())
+        self.soup.find_all.return_value = js_list
+        backup.os.path.join.return_value = 'js path'
+        backup.js_backup(self.soup, self.path)
+
+        mock_ex_file.assert_called_with(js_list[3], 'src', 'js path')
+        self.assertTrue(mock_ex_file.call_count == 5)
+        self.assertTrue(mock_inline_file.call_count == 0)
+        self.assertTrue(mock_stdout.getvalue().endswith('js had backed up.\n'))
 
 
 class MainTestCase(unittest.TestCase):
@@ -42,23 +149,26 @@ class MainTestCase(unittest.TestCase):
         main.os.path.join.return_value = self.path
         main.time.strftime.return_value = 'time'
 
-    def test_mkdir(self):
+    @mock.patch('sys.stdout', new_callable=StringIO)
+    def test_mkdir(self, mock_stdout):
 
         # test the dit has exists
         with mock.patch('main.os.path.exists', return_value=True):
             self.assertEqual(main.mkdir(BACKUP_DIR), self.path)
             main.os.path.join.assert_called_with(BASE_DIR, BACKUP_DIR, 'time')
             self.assertFalse(main.os.makedirs.called)
+            self.assertTrue(mock_stdout.getvalue() == 'doc has existed\n')
 
         # test the dir has not exists
         with mock.patch('main.os.path.exists', return_value=False):
             self.assertEqual(main.mkdir(BACKUP_DIR), self.path)
             main.os.makedirs.assert_called_with(self.path)
-            self.assertEqual(main.os.makedirs.call_count, len(DIRNAMES))
+            self.assertEqual(main.os.makedirs.call_count, len(DIRNAMES) - 1)
 
     @mock.patch('main.mkdir')
     @mock.patch('main.backup')
-    def test_main_back_up(self, mock_backup, mock_mkdir):
+    @mock.patch('sys.stdout', new_callable=StringIO)
+    def test_main_back_up(self, mock_stdout, mock_backup, mock_mkdir):
 
         mock_mkdir.return_value = self.path
         r = mock.Mock()
@@ -68,6 +178,7 @@ class MainTestCase(unittest.TestCase):
         main.BeautifulSoup.return_value = soup
         main.back_up('url', 'backup_dir')
 
+        self.assertTrue(mock_stdout.getvalue() == 'time\n')
         mock_mkdir.assert_called_with('backup_dir')
         main.requests.get.assert_called_with('url')
         main.BeautifulSoup.assert_called_with(r.text, 'html.parser')
